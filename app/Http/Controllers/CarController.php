0d7b02a545b9878efc04b9384bd235718c2e9590
Lucas Heim <lucasedfc@gmail.com>
2019-10-14 18:28:21 -0300

    Register user

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class CarController extends Controller
+{
+    public function index() {
+        
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\User;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function register(Request $req) {
+        $json = $req->input('json');
+        $params = json_decode($json);
+
+        $email = (!is_null($json) && isset($params->email)) ? $params->email : null;
+        $name = (!is_null($json) && isset($params->name)) ? $params->name : null;
+        $surname = (!is_null($json) && isset($params->surname)) ? $params->surname : null;
+        $password = (!is_null($json) && isset($params->password)) ? $params->password : null;
+        $role = 'USER_ROLE';
+
+        if(!is_null($email) && !is_null($name) && !is_null($password)) {
+            //encrypt pass
+            $pwd = hash('sha256', $password);
+
+            $user = new User();
+            $user->email = $email;
+            $user->name = $name;
+            $user->surname = $surname;
+            $user->role = $role;
+            $user->password = $pwd;
+
+            $isset_user = User::where('email', '=', $email)->first();
+
+            if(is_null($isset_user) || (count($isset_user) == 0)) {
+
+                $user->save();
+                
+                $data = array(
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => 'User created'
+                );
+
+            } else {
+                $data = array(
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'User already exist'
+                );
+            }
+
+        } else {
+            $data = array(
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'Error creating user'
+            );
+        }
+
+        return response()->json($data);
+        
+    }
+
+    public function login(Request $req) {
+        
+    }
+}
