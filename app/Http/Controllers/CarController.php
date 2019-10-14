3cbbc3d51a181db657d4a78273c44516dd4c1e66
Lucas Heim <lucasedfc@gmail.com>
2019-10-14 20:04:44 -0300

    Save car with validation

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Helpers\JwtAuth;
 
+use App\Car;
+use Validator;
 
 use App\Http\Requests;
 
@@ -23,5 +25,55 @@ class CarController extends Controller
             echo "Not authenticated";
         }
         
+    }
+
+    public function store(Request $req) {
+        $hash = $req->header('Authorization', null);
+        $jwtAuth = new JwtAuth();
+        $checkToken = $jwtAuth->checkToken($hash);
+
+        if($checkToken) {
+            $json = $req->input('json');
+            $params = json_decode($json);
+            $params_array = json_decode($json, true);
+            //user authenticated
+            $user = $jwtAuth->checkToken($hash, true);
+
+            $valid = Validator::make($params_array, [
+ 
+                'title'=>'required|min:5',
+                'description'=>'required',
+                'price'=>'required',
+                'status'=>'required',
+            ]
+            );
+     
+            if($valid -> fails()){
+                return response()->json($valid->errors(), 400);
+            }
+
+            $car = new Car();
+            $car->user_id = $user->sub;
+            $car->title = $params->title;
+            $car->description = $params->description;
+            $car->status = $params->status;
+            $car->price = $params->price;
+
+            $car->save();
+
+            $data = array(
+                'car' => $car,
+                'status' => 'success',
+                'code' => 200
+            );
+        } else {
+            $data = array(
+                'status' => 'error',
+                'code' => 300,
+                'message' => 'login failed'
+            );
+        }
+
+        return response()->json($data, 200);
     }
 }
